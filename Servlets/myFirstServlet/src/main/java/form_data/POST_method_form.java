9f9f6b2c6d1e2e9d3eaed597aee9932575cedd09
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-08 04:44:34 +0700

    POST Method Example Using Form

--- Servlets/myFirstServlet/src/main/java/form_data/POST_method_form.java
+++ Servlets/myFirstServlet/src/main/java/form_data/POST_method_form.java
@@ -24,40 +24,27 @@ public class POST_method_form extends HttpServlet {
                 "<body bgcolor=\"#f0f0f0\">"+
                 "<h1 align=\"center\">"+title+"</h1>"+
                 "<form action=\"/myFirstServlet/post_method_form\" method=\"get\">\n" +
-                "    <label for=\"username\">Name:</label>\n" +
-                "    <input type=\"text\" id=\"username\" name=\"username\"><br>\n" +
-                "    <label>Gender:</label>\n" +
-                "    <input type=\"radio\" name=\"gender\" value=\"male\"> Male\n" +
-                "    <input type=\"radio\" name=\"gender\" value=\"female\"> Female<br>\n" +
-                "    <input type=\"submit\" value=\"Submit\">\n" +
-                "  </form>\n" +
-                "<br>\n" +
-
-                "\n" +
-                "  <form action=\"/myFirstServlet/post_method_form\" method=\"get\">\n" +
-                "    <label>Marital Status:</label><br>\n" +
-                "    <input type=\"radio\" name=\"marital_status\" value=\"single\"> Single<br>\n" +
-                "    <input type=\"radio\" name=\"marital_status\" value=\"married\"> Married<br>\n" +
-                "    <input type=\"radio\" name=\"marital_status\" value=\"widow\"> Widow<br>\n" +
-                "    <input type=\"radio\" name=\"marital_status\" value=\"prefer_not_to_say\"> Prefer not to say<br>\n" +
-                "    <input type=\"submit\" value=\"Submit\">\n" +
-                "  </form>\n" +
-                "  <br>\n" +
-                "\n" +
-                "  \n" +
-                "  <form action=\"/myFirstServlet/post_method_form\" method=\"get\">\n" +
-                "    <label>Do you have a medical history of diabetes?</label><br>\n" +
-                "    <input type=\"radio\" name=\"diabetes\" value=\"yes\"> Yes<br>\n" +
-                "    <input type=\"radio\" name=\"diabetes\" value=\"no\"> No<br>\n" +
-                "    <input type=\"submit\" value=\"Submit\">\n" +
-                "  </form>\n" +
-                "  <br>\n" +
-                "\n" +
-                "\n" +
-                "  <form action=\"/myFirstServlet/post_method_form\" method=\"get\">\n" +
-                "    <label for=\"feedback\">Your Feedback:</label><br>\n" +
-                "    <textarea id=\"feedback\" name=\"feedback\" rows=\"4\" cols=\"50\"></textarea><br>\n" +
-                "    <input type=\"submit\" value=\"Submit\">\n" +
+                        " <label for=\"username\">Name:</label>\n" +
+                        "    <input type=\"text\" id=\"username\" name=\"username\"><br><br>\n" +
+                        "\n" +
+                        "    <label>Gender:</label>\n" +
+                        "    <input type=\"radio\" name=\"gender\" value=\"male\"> Male\n" +
+                        "    <input type=\"radio\" name=\"gender\" value=\"female\"> Female<br><br><br>\n" +
+                        "\n" +
+                        "    <label>Marital Status:</label><br>\n" +
+                        "    <input type=\"radio\" name=\"marital_status\" value=\"single\"> Single<br>\n" +
+                        "    <input type=\"radio\" name=\"marital_status\" value=\"married\"> Married<br>\n" +
+                        "    <input type=\"radio\" name=\"marital_status\" value=\"widow\"> Widow<br>\n" +
+                        "    <input type=\"radio\" name=\"marital_status\" value=\"prefer_not_to_say\"> Prefer not to say<br><br><br>\n" +
+                        "\n" +
+                        "    <label>Do you have a medical history of diabetes?</label><br>\n" +
+                        "    <input type=\"radio\" name=\"diabetes\" value=\"yes\"> Yes<br>\n" +
+                        "    <input type=\"radio\" name=\"diabetes\" value=\"no\"> No<br><br><br>\n" +
+                        "\n" +
+                        "    <label for=\"feedback\">Your Feedback:</label><br>\n" +
+                        "    <textarea id=\"feedback\" name=\"feedback\" rows=\"4\" cols=\"50\"></textarea><br><br><br>\n" +
+                        "\n" +
+                        "    <input type=\"submit\" value=\"Submit\">"+
                 "  </form>\n" +
                 "  <br><br><br>\n"+
                 "</body>"+
