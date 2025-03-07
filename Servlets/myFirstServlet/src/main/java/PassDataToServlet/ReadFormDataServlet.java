3c7927133be04162ae5b7d51ab2b5506f703c2a4
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-08 04:46:04 +0700

    Passing data to the servlet

---
+++ Servlets/myFirstServlet/src/main/java/PassDataToServlet/ReadFormDataServlet.java
@@ -0,0 +1,61 @@
+package PassDataToServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/readFormData")
+public class ReadFormDataServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String title = "Reading Form Data";
+        String docType = "<!DOCTYPE html>\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body style=\"background-color: #f0f0f0;\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n");
+
+        // Reading text input
+        String username = request.getParameter("username");
+        out.println("<li><b>Name:</b> " + (username != null ? username : "Not provided") + "</li>\n\n");
+
+        // Reading radio button (gender)
+        String gender = request.getParameter("gender");
+        out.println("<li><b>Gender:</b> " + (gender != null ? gender : "Not selected") + "</li>\n\n");
+
+        // Reading radio button (marital status)
+        String maritalStatus = request.getParameter("marital_status");
+        out.println("<li><b>Marital Status:</b> " + (maritalStatus != null ? maritalStatus : "Not selected") + "</li>\n\n");
+
+        // Reading radio button (diabetes history)
+        String diabetes = request.getParameter("diabetes");
+        out.println("<li><b>Diabetes History:</b> " + (diabetes != null ? diabetes : "Not provided") + "</li>\n\n");
+
+        // Reading textarea (feedback)
+        String feedback = request.getParameter("feedback");
+        out.println("<li><b>Feedback:</b> " + (feedback != null ? feedback : "No feedback given") + "</li>\n\n");
+
+        out.println("</ul>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
--- Servlets/myFirstServlet/src/main/webapp/index.jsp
+++ Servlets/myFirstServlet/src/main/webapp/index.jsp
@@ -1,5 +1,33 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h2>Enter Your Details:</h2>
+<form action="readFormData" method="get">
+    <label for="username">Name:</label>
+    <input type="text" id="username" name="username"><br><br>
+
+    <label>Gender:</label>
+    <input type="radio" name="gender" value="male"> Male
+    <input type="radio" name="gender" value="female"> Female<br><br><br>
+
+    <label>Marital Status:</label><br>
+    <input type="radio" name="marital_status" value="single"> Single<br>
+    <input type="radio" name="marital_status" value="married"> Married<br>
+    <input type="radio" name="marital_status" value="widow"> Widow<br>
+    <input type="radio" name="marital_status" value="prefer_not_to_say"> Prefer not to say<br><br><br>
+
+    <label>Do you have a medical history of diabetes?</label><br>
+    <input type="radio" name="diabetes" value="yes"> Yes<br>
+    <input type="radio" name="diabetes" value="no"> No<br><br><br>
+
+    <label for="feedback">Your Feedback:</label><br>
+    <textarea id="feedback" name="feedback" rows="4" cols="50"></textarea><br><br><br>
+
+    <input type="submit" value="Submit">
+</form>
 </body>
 </html>
