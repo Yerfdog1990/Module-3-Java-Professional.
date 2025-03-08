09caccbdacac183f88b904b041dcbb0051e000b8
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-08 09:40:27 +0700

    HTTP Header Request Example

---
+++ Servlets/myFirstServlet/src/main/java/ClientRequest/DisplayHeader.java
@@ -0,0 +1,47 @@
+package ClientRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+
+@WebServlet("/displayHeader")
+public class DisplayHeader extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        String title = "HTTP Header Request Example";
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+        out.println(docType +
+                "<html>" +
+                "<head><title>" + title + "</title></head>" +
+                "<body bgcolor=\"#f0f0f0\">" +
+                "<h1 align=\"center\">" + title + "</h1>" +
+                "<table  width=\"100\" border=\"1\" align=\"center\">" +
+                "<tr><th>Header Name</th><th>Header Value</th></tr>\n"
+        );
+
+        // Logic to read the HTTP header information
+        Enumeration headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = (String) headerNames.nextElement();
+            out.println("<tr bgcolor = \"#949494\"><td>" + headerName + "</td>\n");
+            String headerValue = req.getHeader(headerName);
+            out.println("<td>" + headerValue + "</td></tr>\n");
+        }
+        out.println("</table>" +
+                "</body>" +
+                "</html>\n"
+        );
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
