6e045d43d2cf5694442a26e6371cdd323675bbb4
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-08 15:42:15 +0700

    Filtering in servlet

--- Servlets/myFirstServlet/src/main/java/Filtering/FilteringServlet.java
+++ Servlets/myFirstServlet/src/main/java/Filtering/FilteringServlet.java
@@ -1,4 +1,14 @@
 package Filtering;
 
-public class FilteringServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+@WebServlet("/filtering")
+public class FilteringServlet extends HttpServlet {
+    @Override
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.getWriter().write("Welcome to servlet");
+    }
 }
