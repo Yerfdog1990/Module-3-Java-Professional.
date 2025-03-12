20642060fcdd2937aad8ae98985b22061a93670c
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-12 20:43:20 +0700

    Adapter pattern

---
+++ Design-Patterns/GoF/src/main/java/pattern/adapter/Log4JLogger.java
@@ -0,0 +1,28 @@
+package pattern.adapter;
+
+// Adaptee
+public class Log4JLogger {
+  public void logInfo(String msg) {
+    System.out.println("Log4JLogger: " + msg);
+  }
+
+  public void logError(String msg) {
+    System.out.println("Log4JLogger: " + msg);
+  }
+
+  public void logDebug(String msg) {
+    System.out.println("Log4JLogger: " + msg);
+  }
+
+  public void logWarn(String msg) {
+    System.out.println("Log4JLogger: " + msg);
+  }
+
+  public void logTrace(String msg) {
+    System.out.println("Log4JLogger: " + msg);
+  }
+
+  public void logFatal(String msg) {
+    System.out.println("Log4JLogger: " + msg);
+  }
+}
---
+++ Design-Patterns/GoF/src/main/java/pattern/adapter/Log4JSLF4UtilAdapter.java
@@ -0,0 +1,39 @@
+package pattern.adapter;
+
+public class Log4JSLF4UtilAdapter implements Logger {
+  private final Log4JUtilLogger logger;
+
+  public Log4JSLF4UtilAdapter(Log4JUtilLogger logger) {
+    this.logger = logger;
+  }
+
+  @Override
+  public void info(String msg) {
+    logger.logInfo(msg);
+  }
+
+  @Override
+  public void error(String msg) {
+    logger.logError(msg);
+  }
+
+  @Override
+  public void debug(String msg) {
+    logger.logDebug(msg);
+  }
+
+  @Override
+  public void warn(String msg) {
+    logger.logWarn(msg);
+  }
+
+  @Override
+  public void trace(String msg) {
+    logger.logTrace(msg);
+  }
+
+  @Override
+  public void fatal(String msg) {
+    logger.logFatal(msg);
+  }
+}
