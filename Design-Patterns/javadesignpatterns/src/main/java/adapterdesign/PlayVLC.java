6bd5bd5606ccdb5b7d584fea482d0d3f096d47f8
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-13 19:44:50 +0700

    Adapter pattern.

---
+++ Design-Patterns/javadesignpatterns/src/main/java/adapterdesign/PlayVLC.java
@@ -0,0 +1,13 @@
+package adapterdesign;
+
+public class PlayVLC implements AdvancedMediaPlayer {
+    @Override
+    public void playVlc(String fileName) {
+        System.out.println("Playing vlc file. Name: " + fileName);
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+        // Do nothing
+    }
+}
