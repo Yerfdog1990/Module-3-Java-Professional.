3086af037a834fe7e07bf24255addc8663af6141
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-23 15:58:14 +0700

    using @ThreadSafe and @GuardedBy concurrency annotations

--- Concurrency/part-1-fundamentals/src/main/java/concurrencyannotations/SafeSequence.java
+++ Concurrency/part-1-fundamentals/src/main/java/concurrencyannotations/SafeSequence.java
@@ -1,4 +1,33 @@
 package concurrencyannotations;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe // The synchronized keyword ensures that only one thread at a time modifies count.
 public class SafeSequence {
+    @GuardedBy("this") private int count = 0;
+    public synchronized void getCount() {
+        System.out.println(count++); // Now thread-safe!
+    }
+    public static void main(String[] args) {
+        SafeSequence sequence = new SafeSequence();
+        Thread thread1 = new Thread(()->{
+            for(int i=0; i<10; i++) {
+                sequence.getCount();
+            }
+        });
+        Thread thread2 = new Thread(()->{
+            for(int i=0; i<10; i++) {
+                sequence.getCount();
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
 }
+/*
+@ThreadSafe
+📌 Indicates that a class is thread-safe.
+Implies the class has built-in synchronization to handle multiple threads safely.
+Helps developers use the class with confidence in concurrent programs.
+ */
