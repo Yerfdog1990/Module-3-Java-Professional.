f9765093111b11280b813fb9630d050041694685
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-30 15:45:00 +0700

    Handling equals() with Inheritance.

--- Apache-commons/apache-commons-demo/src/main/java/org/example/equalsbuilder/EqualsWithInheritance.java
+++ Apache-commons/apache-commons-demo/src/main/java/org/example/equalsbuilder/EqualsWithInheritance.java
@@ -1,4 +1,50 @@
 package org.example.equalsbuilder;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 public class EqualsWithInheritance {
+  public static void main(String[] args) {
+    Dog dog1 = new Dog("Canis lupus familiaris", "Bulldog");
+    Dog dog2 = new Dog("Canis lupus familiaris", "Bulldog");
+    System.out.println("dog1.equals(dog2): " + dog1.equals(dog2));
+    System.out.println("dog1 == dog2: " + (dog1 == dog2));
+  }
+}
+
+class Animal {
+  protected String species;
+
+  public Animal(String species) {
+    this.species = species;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+
+    Animal other = (Animal) obj;
+    return new EqualsBuilder().append(species, other.species).isEquals();
+  }
+}
+
+class Dog extends Animal {
+  private String breed;
+
+  public Dog(String species, String breed) {
+    super(species);
+    this.breed = breed;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null || obj.getClass() != getClass()) return false;
+
+    Dog other = (Dog) obj;
+    return new EqualsBuilder()
+        .appendSuper(super.equals(obj)) // Include superclass comparison
+        .append(breed, other.breed)
+        .isEquals();
+  }
 }
