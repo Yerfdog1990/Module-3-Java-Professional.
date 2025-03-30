4261bcbb72270580da1ece5524caff3b06b9d20b
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-30 16:08:43 +0700

    Excluding Fields with @EqualsExclude

---
+++ Apache-commons/apache-commons-demo/src/main/java/org/example/equalsbuilder/ExcludingFields.java
@@ -0,0 +1,45 @@
+package org.example.equalsbuilder;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.EqualsExclude;
+
+public class ExcludingFields {
+  public static void main(String[] args) {
+    PersonInfo p1 = new PersonInfo("John", 20);
+    PersonInfo p2 = new PersonInfo("John", 20);
+    System.out.println("p1.equals(p2): " + p1.equals(p2));
+    System.out.println("p1 == p2: " + (p1 == p2));
+  }
+}
+
+class PersonInfo {
+  private final String name;
+  private final int age;
+
+  @EqualsExclude private transient String address;
+
+  public PersonInfo(String name, int age) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+
+    PersonInfo other = (PersonInfo) obj;
+    return new EqualsBuilder().append(name, other.name).append(age, other.age).isEquals();
+  }
+}
+
+/*
+Excluding Fields with @EqualsExclude
+Sometimes, you may want to exclude certain fields from comparison. You can use @EqualsExclude.
+
+Why Use @EqualsExclude?
+1.Prevents non-important fields from affecting equality.
+2.Useful for transient or dynamically changing fields.
+ */
