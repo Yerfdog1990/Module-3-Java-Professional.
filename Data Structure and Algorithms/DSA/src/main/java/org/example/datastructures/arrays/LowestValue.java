28914808aa6cc1f8aee92d8c44803fc1e4b255e4
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-04-05 14:03:30 +0700

    Search for the lowest value in the array.

---
+++ Data Structure and Algorithms/DSA/src/main/java/org/example/datastructures/arrays/LowestValue.java
@@ -0,0 +1,26 @@
+package org.example.datastructures.arrays;
+
+/*
+How it works:
+1.Create a variable 'minVal' and set it equal to the first value of the array.
+2.Go through every element in the array.
+3.If the current element has a lower value than 'minVal', update 'minVal' to this value.
+4.After looking at all the elements in the array, the 'minVal' variable now contains the lowest value.
+ */
+public class LowestValue {
+  private static int[] intArray = {19, 22, 0, 13, 4, 35};
+  private static int minVal = intArray[0];
+
+  private static int printMinVal(int[] intArray) {
+    for (int i = 0; i < intArray.length; i++) {
+      if (intArray[i] < minVal) {
+        minVal = intArray[i];
+      }
+    }
+    return minVal;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Lowest value in the array is: " + printMinVal(intArray));
+  }
+}
